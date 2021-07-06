719deb6b245571fb4842bc84296c590d7ba4518b
saleem-ux <saleem_diab86@yahoo.com>
2021-07-06 06:42:17 -0700

    lab06

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button } from 'react-bootstrap'
+
 
 class App extends React.Component {
 
@@ -9,42 +12,59 @@ class App extends React.Component {
     this.state = {
       LocationObject: {},
       userCityInput: '',
-      showMap: false
+      showMap: false,
+      showError: false
     }
   }
 
   getLocation = async (event) => {
     event.preventDefault();
-
     await this.setState({
       userCityInput: event.target.city.value
     })
-
+    
     console.log('user input', this.state.userCityInput)
-
+    
     let url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.userCityInput}&format=json`;
+    
+    try {
+      let getData = await axios.get(url);
 
-    let getData = await axios.get(url);
+      console.log(getData)
+      console.log(getData.data)
+      console.log(getData.data[0])
 
-    console.log(getData)
-    console.log(getData.data)
-    console.log(getData.data[0])
 
-    this.setState({
-      LocationObject: getData.data[0],
-      showMap: true
-    })
+      this.setState({
+        LocationObject: getData.data[0],
+        showError: false,
+        showMap: true
+      })
 
+    } catch (err) {
+      this.setState({
+        showError: true,
+      })
+    }
   }
 
   render() {
     return (
       <div>
         <h1>City Explorer</h1>
-        <form onSubmit={this.getLocation}>
+        {/* <form onSubmit={this.getLocation}>
           <input type='text' placeholder='city name' name='city' />
           <input type='submit' value='get City data' />
-        </form>
+        </form> */}
+        <Form onSubmit={this.getLocation}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>City Explorer</Form.Label>
+            <Form.Control type="text" placeholder="City Name" name='city' />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
 
         <p>City Name: {this.state.LocationObject.display_name},{this.state.LocationObject.lat},{this.state.LocationObject.lon}</p>
 
